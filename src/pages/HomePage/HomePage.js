44a4ef68a2496752792489c5989f819271084753
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-04-02 19:09:33 +0300

    добавляет кнопку подгрузки  и коментит консоли

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,57 +2,79 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchTrendingMovies } from '../../services/movieApi/movieApi';
 import hps from './HomePage.module.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
+import Button from '../../components/Button/Button';
 
 class HomePage extends Component {
   state = {
     movies: [],
     page: 1,
     error: false,
+    fetchLength: 0,
+    isLoading: false,
   };
   componentDidMount() {
     // console.log('Я родился!');
     this.getTrendingMovies();
     // console.log(this.state);
   }
-  // componentDidUpdate() {
-  //   console.log('Я обновился!');
-  //   // const { movies } = this.state;
-  //   // console.log(movies);
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('Я обновился!');
+    const { page } = this.state;
+    // console.log(movies);
+    if (page !== prevState.page) {
+      this.getTrendingMovies();
+    }
+  }
   // componentWillUnmount() {
   //   console.log('Я умер!');
   // }
   getTrendingMovies = (prevProps, prevState) => {
     const { page } = this.state;
 
-    // this.setState({ isLoading: true });
+    this.setState({ isLoading: true });
     fetchTrendingMovies(page)
       .then(({ results }) => {
         // console.log(results);
-        this.setState({
-          movies: results,
-          // fetchLength: result.length,
-        });
+        this.setState(prevState => ({
+          movies: [...prevState.movies, ...results],
+          fetchLength: results.length,
+        }));
       })
       .catch(error => {
         this.setState({ error: true });
       })
       .finally(() => {
-        // this.setState({ isLoading: false });
+        this.setState({ isLoading: false });
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: 'smooth',
         });
       });
-    // }
+  };
+  setNewPage = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
   render() {
-    const { movies } = this.state;
+    const { movies, isLoading, fetchLength } = this.state;
     const { location } = this.props;
-    console.log(location);
+    // console.log(this.state);
     return (
       <div>
         <h1>Trending today</h1>
+        {isLoading && (
+          <Loader
+            type="ThreeDots"
+            color="#3f51b5"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        )}
         <ul className={hps.movieList}>
           {movies &&
             movies.map(movie => {
@@ -71,6 +93,9 @@ class HomePage extends Component {
               );
             })}
         </ul>
+        {fetchLength === 20 && !isLoading && (
+          <Button getNewPage={this.setNewPage} />
+        )}
       </div>
     );
   }
--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -5,6 +5,7 @@ import hps from '../HomePage/HomePage.module.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Loader from 'react-loader-spinner';
 import Searchbar from '../../components/Searchbar/Searchbar';
+import Button from '../../components/Button/Button';
 
 class MoviesPage extends Component {
   state = {
@@ -12,6 +13,7 @@ class MoviesPage extends Component {
     page: 1,
     query: '',
     error: false,
+    fetchLength: 0,
     isLoading: false,
   };
   componentDidMount() {
@@ -27,7 +29,7 @@ class MoviesPage extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // console.log('Я обновился MoviesPage!');
+    console.log('Я обновился MoviesPage!');
     // console.log(this.state);
 
     const { query, page } = this.state;
@@ -58,25 +60,44 @@ class MoviesPage extends Component {
     fetchMoviesByQuery(query, page)
       .then(({ results }) => {
         // console.log(results);
-        this.setState({
-          movies: results,
-        });
+        this.setState(prevState => ({
+          movies: [...prevState.movies, ...results],
+          fetchLength: results.length,
+        }));
       })
       .catch(error => {
         this.setState({ error: true });
       })
       .finally(() => {
         this.setState({ isLoading: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
       });
   };
+  setNewPage = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
   render() {
-    const { movies, isLoading } = this.state;
+    const { movies, isLoading, fetchLength } = this.state;
     // console.log(this.props.match.url);
     const { location } = this.props;
     return (
       <div>
         <Searchbar onSubmit={this.getQueryByForm} />
-
+        {isLoading && (
+          <Loader
+            type="ThreeDots"
+            color="#3f51b5"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        )}
         <ul className={hps.movieList}>
           {movies &&
             movies.map(movie => {
@@ -95,14 +116,9 @@ class MoviesPage extends Component {
               );
             })}
         </ul>
-        {isLoading && (
-          <Loader
-            type="ThreeDots"
-            color="#3f51b5"
-            height={100}
-            width={100}
-            timeout={3000}
-          />
+
+        {fetchLength === 20 && !isLoading && (
+          <Button getNewPage={this.setNewPage} />
         )}
       </div>
     );
